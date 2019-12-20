4da6faf82c9fd98df818110f9ad789e6ca4f1094
Jasper <asd01248967@hotmail.com>
2019-12-20 17:44:09 +0800

    try

--- file1.py
+++ file1.py
@@ -1,6 +1,5 @@
 #fruits = ['apple', 'cherry', 'pear', 'banana','strawberry']
 fruits = ['apple']
-blank = []
 dict = {'a':1, 'p':2, 'l':3, 'e':4, 'c':5, 'h':6, 'r':7, 'y':8, 'b':9, 'n':10, 's':11, 't':12, 'w':13}
 for split in fruits:
     split_list = list(split)
@@ -8,8 +7,7 @@ for split in fruits:
     split_first = split_list[0]
     #number = split_lenght - 1
     #split_end = split_list[number]
+    a = 0
     for total in range(0, split_lenght):
-        a = dict[split_list[total]]
-        i = 0
-        while i=split_lenght:
-            pass
+        a += dict[split_list[total]]
+    print(a)

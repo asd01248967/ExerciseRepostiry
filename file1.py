9609cb609b0f80524f89ec0be3406f8178bafe1d
Jasper <asd01248967@hotmail.com>
2019-12-20 16:23:33 +0800

    字典內新增字母與值對應

--- file1.py
+++ file1.py
@@ -1,16 +1,11 @@
-fruits = ['apple', 'cherry', 'pear', 'banana','strawberry']
+#fruits = ['apple', 'cherry', 'pear', 'banana','strawberry']
+fruits = ['apple']
+dict = {'a':1, 'p':2, 'l':3, 'e':4, 'c':5, 'h':6, 'r':7, 'y':8, 'b':9, 'n':10, 's':11, 't':12, 'w':13}
 for split in fruits:
     split_list = list(split)
     split_lenght = len(split)
     split_first = split_list[0]
     number = split_lenght - 1
     split_end = split_list[number]
-    print(split_first)
-    print(split_end)
-    # for test in range(split_first, split_lenght):
-    #     pass
-    # else:
-    #     pass
-
-
-#list元素是從0開始的，所以長度要-1才會是最後的item
+    for total in range(0, split_lenght):
+        print(dict[split_list[total]])

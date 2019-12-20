0fe8a16e0d789c92f56d89fc1f1e9e3c70ef79d8
Jasper <asd01248967@hotmail.com>
2019-12-20 18:42:31 +0800

    大致上資料處理功能完成，剩餘excel匯入

--- file1.py
+++ file1.py
@@ -1,10 +1,12 @@
-fruits = ['apple', 'cherry', 'pear', 'banana','strawberry']
-dict = {'a':1, 'p':2, 'l':3, 'e':4, 'c':5, 'h':6, 'r':7, 'y':8, 'b':9, 'n':10, 's':11, 't':12, 'w':13}
+fruits = ['apple', 'cherry', 'pear', 'banana', 'strawberry', 'whiskey', 'intellect', 'chimpanzee', 'grape', 'attitude']
+dict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26,}
+match = []
 for split in fruits:
     split_list = list(split)
     split_lenght = len(split)
-    #split_first = split_list[0]
     a = 0
     for total in range(0, split_lenght):
         a += dict[split_list[total]]
-    print(a)
+    if a == 100:
+        match.append(split)
+print(match)

8505705a3f0eb4ce0b705759caca49b2f66a6d30
Jasper <asd01248967@hotmail.com>
2019-12-20 15:50:11 +0800

    思考

--- file1.py
+++ file1.py
@@ -1,6 +1,14 @@
 fruits = ['apple', 'cherry', 'pear', 'banana','strawberry']
 for split in fruits:
-    print(list(split))
+    split_list = list(split)
     split_lenght = len(split)
-    print(split_lenght)
-    print(type(split_lenght))
+    split_first = split[0]
+    split_end = split[split_lenght]
+    print(split_list[0])
+    # for test in range(split_first, split_lenght):
+    #     pass
+    # else:
+    #     pass
+
+
+#list元素是從0開始的，所以長度要-1才會是最後的item

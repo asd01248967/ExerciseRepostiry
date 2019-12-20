7bf99e95b6b8deded65b703e15671323ab8de46d
Jasper <asd01248967@hotmail.com>
2019-12-20 12:09:48 +0800

    拆分單字；拆分字母；轉換型態；檢查長度

--- file1.py
+++ file1.py
@@ -1,4 +1,5 @@
 fruits = ['apple', 'cherry', 'pear', 'banana','strawberry']
 for split in fruits:
-    print(type(list(split)))
     print(list(split))
+    split_lenght = len(split)
+    print(split_lenght)

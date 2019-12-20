99959bdaa196a273e3d46b7aeb376c3e2a7a3d8c
Jasper <asd01248967@hotmail.com>
2019-12-20 18:05:06 +0800

    試著加總五個單字

--- file1.py
+++ file1.py
@@ -1,12 +1,9 @@
-#fruits = ['apple', 'cherry', 'pear', 'banana','strawberry']
-fruits = ['apple']
+fruits = ['apple', 'cherry', 'pear', 'banana','strawberry']
 dict = {'a':1, 'p':2, 'l':3, 'e':4, 'c':5, 'h':6, 'r':7, 'y':8, 'b':9, 'n':10, 's':11, 't':12, 'w':13}
 for split in fruits:
     split_list = list(split)
     split_lenght = len(split)
-    split_first = split_list[0]
-    #number = split_lenght - 1
-    #split_end = split_list[number]
+    #split_first = split_list[0]
     a = 0
     for total in range(0, split_lenght):
         a += dict[split_list[total]]

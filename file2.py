a4d0057013cfb1038d3b51d488c4125470652272
Jasper <asd01248967@hotmail.com>
2019-12-30 19:01:38 +0800

    嘗試更換頁籤

--- file2.py
+++ file2.py
@@ -3,19 +3,26 @@ import time
 tStart = time.time()
 import openpyxl
 
-book = openpyxl.load_workbook('C:\\Users\\Jasper\\Desktop\\100.xlsx')
+book = openpyxl.load_workbook('C:\\Users\\Jasper\\Desktop\\7000.xlsx')
 
 sheet = book.active
 
+sheetlist = book.sheetnames
+
+for sheettest in sheetlist:
+    print(sheettest)
+
 newlist = []
 
-for number in range(1,10):
+for number in range(1,2000):
     if sheet.cell(row=number, column=1).value == None:
         pass
     else:
         newlist.append(sheet.cell(row=number, column=1).value)
-        print(newlist)
+
 
 tEnd = time.time()
-print("It cost %f sec" % (tEnd - tStart))
-print(tEnd - tStart)
+# print(len(newlist))
+# print(newlist[1248])
+# print("It cost %f sec" % (tEnd - tStart))
+# print(tEnd - tStart)

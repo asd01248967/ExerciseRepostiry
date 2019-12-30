be59ed615e1d403aad45fd814929f762afad6044
Jasper <asd01248967@hotmail.com>
2019-12-30 10:30:55 +0800

    123

--- file1.py
+++ file1.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from collections import OrderedDict
 dataframe = pd.read_excel(io='C:\\Users\\Jasper\\Desktop\\Book1.xlsx',sheet_name=None,header=None)
+print(dataframe)
 
-abc= list(dataframe.items())
 
-# fruits = ['apple', 'cherry', 'pear', 'banana', 'strawberry', 'whiskey', 'intellect', 'chimpanzee', 'grape', 'attitude']
+vocabulary= list(dataframe.items())
+
+#fruits = ['apple', 'cherry', 'pear', 'banana', 'strawberry', 'whiskey', 'intellect', 'chimpanzee', 'grape', 'attitude']
 #dict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26,}
 #match = []
 
-for split in list(dataframe.items()):
-    print(split)
-    split_list = list(split)
-    split_lenght = len(split)
+
+for split2 in vocabulary:
+    print(len(split2))
 
 # for split in fruits:
+#     print(type(split))
 #     split_list = list(split)
 #     split_lenght = len(split)
 #     a = 0
---
+++ file2.py
@@ -0,0 +1,12 @@
+# from collections import OrderedDict
+# od = OrderedDict(enumerate("abcdefg"))
+# print(od)
+
+# print(type(od))
+
+import pandas as pd
+dataframe = pd.read_csv('spreadsheet.csv')
+#od = enumerate(dataframe.items())
+#od2 = (dataframe.items())
+print(type(dataframe))
+

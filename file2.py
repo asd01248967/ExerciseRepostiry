d194c2b14d220ce351aed597cffd7f99e0ec9702
Jasper <asd01248967@hotmail.com>
2019-12-30 14:24:33 +0800

    被pandas打敗，換另一個新套件

--- file2.py
+++ file2.py
@@ -1,12 +1,16 @@
-# from collections import OrderedDict
-# od = OrderedDict(enumerate("abcdefg"))
-# print(od)
+# import pandas as pd
+# dataframe = pd.read_excel(io='C:\\Users\\Jasper\\Desktop\\10.xlsx',sheet_name=None,header=None)
+# content = list(dataframe)
+# print(content)
 
-# print(type(od))
+#匯入openpyxl套件庫
+from openpyxl import load_workbook
+#透過load_workbook讀取xlsx檔案為wb
+wb = load_workbook(filename="C:\\Users\\Jasper\\Desktop\\10.xlsx")
+#設定sheets為str內容元素類型的list，類似['1級', '2級', '3級', '4級', '5級', '6級']
+sheets = (wb.sheetnames)
+#取得頁籤的內容
+ws = wb[sheets[0]]
 
-import pandas as pd
-dataframe = pd.read_csv('spreadsheet.csv')
-#od = enumerate(dataframe.items())
-#od2 = (dataframe.items())
-print(type(dataframe))
-
+print(ws)
+print(type(ws))

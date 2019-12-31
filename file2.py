f41f74f179b5b1e2ff925734b1521af826bf18ab
Jasper <asd01248967@hotmail.com>
2019-12-31 11:23:19 +0800

    完成worksheet切換以及取值

--- file2.py
+++ file2.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
-import time
-tStart = time.time()
+# import time
+# tStart = time.time()
 import openpyxl
-
-book = openpyxl.load_workbook('C:\\Users\\Jasper\\Desktop\\7000.xlsx')
-
-sheet = book.active
-
-sheetlist = book.sheetnames
-
-for sheettest in sheetlist:
-    print(sheettest)
 
 newlist = []
 
-for number in range(1,2000):
-    if sheet.cell(row=number, column=1).value == None:
-        pass
-    else:
-        newlist.append(sheet.cell(row=number, column=1).value)
+book = openpyxl.load_workbook('C:\\Users\\Jasper\\Desktop\\7000.xlsx')
 
+#sheet = book.active
+####################################
 
-tEnd = time.time()
+sheetlist = book.sheetnames
+
+for s_name in sheetlist:
+    sheet = book[s_name]
+    #print(sheet.max_row)
+    for number in range(1,sheet.max_row+1):
+        if sheet.cell(row=number, column=1).value == None:
+            pass
+        else:
+            newlist.append(sheet.cell(row=number, column=1).value)
+    print(len(newlist))
+    print(newlist[-1])
+
+            
+
+####################################
+#tEnd = time.time()
+
 # print(len(newlist))
 # print(newlist[1248])
 # print("It cost %f sec" % (tEnd - tStart))

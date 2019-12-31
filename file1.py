267385346bf37c73b318fae8557a761adc3cfe78
Jasper <asd01248967@hotmail.com>
2019-12-31 19:02:24 +0800

    測試list字串內的元素是否重複

--- file1.py
+++ file1.py
@@ -1,21 +1,21 @@
-# import pandas as pd
-# from collections import OrderedDict
-# dataframe = pd.read_excel(io='C:\\Users\\Jasper\\Desktop\\10.xlsx',sheet_name="1級",header=None)
-# print(dataframe)
+listOfElems = ['Hello', 'Ok', 'is', 'Ok', 'test', 'this', 'is', 'a', 'test']
+test = set(listOfElems)
+len1 = len(listOfElems)
+len2 = len(test)
 
 
 
+def checkIfDuplicates_1(listOfElems):
+#''' Check if given list contains any duplicates '''
+if len1 == len2:
+    print('yes')
+else:
+    print('no')
 
-fruits = ['apple', 'cherry', 'pear', 'banana', 'strawberry', 'whiskey', 'intellect', 'chimpanzee', 'grape', 'attitude']
-dict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26,}
-match = []
 
-for split in fruits:
-    split_list = list(split)
-    split_lenght = len(split)
-    a = 0
-    for total in range(0, split_lenght):
-        a += dict[split_list[total]]
-    # if a == 100:
-    #     match.append(split)
-print(match)
+result = checkIfDuplicates_1(listOfElems)
+
+if result:
+    print('Yes, list contains duplicates')
+else:
+    print('No duplicates found in list')    

8bb2673e1f4a5ae4b530e6c6c44eb734030ee68e
Jasper <asd01248967@hotmail.com>
2019-12-31 14:20:51 +0800

    使用正規表達式篩選@前的英文字母

--- file1.py
+++ file1.py
@@ -1,26 +1,21 @@
-import pandas as pd
-from collections import OrderedDict
-dataframe = pd.read_excel(io='C:\\Users\\Jasper\\Desktop\\10.xlsx',sheet_name="1級",header=None)
-print(dataframe)
+# import pandas as pd
+# from collections import OrderedDict
+# dataframe = pd.read_excel(io='C:\\Users\\Jasper\\Desktop\\10.xlsx',sheet_name="1級",header=None)
+# print(dataframe)
 
 
-vocabulary= list(dataframe.items())
-
-#fruits = ['apple', 'cherry', 'pear', 'banana', 'strawberry', 'whiskey', 'intellect', 'chimpanzee', 'grape', 'attitude']
-#dict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26,}
-#match = []
 
 
-for split2 in vocabulary:
-    print(len(split2))
+fruits = ['apple', 'cherry', 'pear', 'banana', 'strawberry', 'whiskey', 'intellect', 'chimpanzee', 'grape', 'attitude']
+dict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26,}
+match = []
 
-# for split in fruits:
-#     print(type(split))
-#     split_list = list(split)
-#     split_lenght = len(split)
-#     a = 0
-#     for total in range(0, split_lenght):
-#         a += dict[split_list[total]]
-#     if a == 100:
-#         match.append(split)
-# print(match)
+for split in fruits:
+    split_list = list(split)
+    split_lenght = len(split)
+    a = 0
+    for total in range(0, split_lenght):
+        a += dict[split_list[total]]
+    # if a == 100:
+    #     match.append(split)
+print(match)
--- file2.py
+++ file2.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python
-# import time
-# tStart = time.time()
+import time
+tStart = time.time()
 import openpyxl
+import re
 
 newlist = []
 
-book = openpyxl.load_workbook('C:\\Users\\Jasper\\Desktop\\7000.xlsx')
+match = []
 
-#sheet = book.active
-####################################
+letter = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26,}
+
+book = openpyxl.load_workbook('C:\\Users\\Jasper\\Desktop\\7000.xlsx')
 
 sheetlist = book.sheetnames
 
 for s_name in sheetlist:
     sheet = book[s_name]
-    #print(sheet.max_row)
     for number in range(1,sheet.max_row+1):
         if sheet.cell(row=number, column=1).value == None:
             pass
         else:
             newlist.append(sheet.cell(row=number, column=1).value)
-    print(len(newlist))
-    print(newlist[-1])
 
-            
 
-####################################
-#tEnd = time.time()
 
-# print(len(newlist))
-# print(newlist[1248])
-# print("It cost %f sec" % (tEnd - tStart))
-# print(tEnd - tStart)
+
+for spilt in newlist:
+    #list長度是8799，但是從0~8798格才有value
+    alphabet = list(spilt)
+    alphabetlen = len(spilt)
+    a = 0
+    # for total in range(0, alphabetlen):
+    #     if alphabet[total] == @ :
+    #         pass
+    #     a += letter[alphabet[total]]
+    # if a > 200:
+    #     print(a)
+        
+#print(newlist[4657])
+
+tEnd = time.time()
+
+print("It cost %f sec" % (tEnd - tStart))

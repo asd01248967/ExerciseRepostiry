b3f22af7edaacb8e52a3df77f9444bafed76a8bf
Jasper <asd01248967@hotmail.com>
2020-01-02 10:47:48 +0800

    檢查是否在list有包含重複的元素

--- file1.py
+++ file1.py
@@ -1,21 +1,34 @@
 listOfElems = ['Hello', 'Ok', 'is', 'Ok', 'test', 'this', 'is', 'a', 'test']
+print(listOfElems)
+print(type(listOfElems))
+print(len(listOfElems))
+
 test = set(listOfElems)
-len1 = len(listOfElems)
-len2 = len(test)
 
+print(test)
+print(type(test))
+print(len(test))
 
-
-def checkIfDuplicates_1(listOfElems):
-#''' Check if given list contains any duplicates '''
-if len1 == len2:
-    print('yes')
+if len(listOfElems) == len(test):
+    print('No duplicates found in list')
 else:
-    print('no')
-
-
-result = checkIfDuplicates_1(listOfElems)
-
-if result:
     print('Yes, list contains duplicates')
-else:
-    print('No duplicates found in list')    
+# len1 = len(listOfElems)
+# len2 = len(test)
+
+
+
+# def checkIfDuplicates_1(listOfElems):
+# #''' Check if given list contains any duplicates '''
+# if len1 == len2:
+#     print('yes')
+# else:
+#     print('no')
+
+
+# result = checkIfDuplicates_1(listOfElems)
+
+# if result:
+#     print('Yes, list contains duplicates')
+# else:
+#     print('No duplicates found in list')    

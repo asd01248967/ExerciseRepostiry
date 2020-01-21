c965015ff6a17aab2c1650b5a79c396b25f04c47
Jasper <asd01248967@hotmail.com>
2020-01-21 15:01:19 +0800

    轉成set時候才發生亂數的情況

--- sort.py
+++ sort.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
 import openpyxl
 import re
+
 newlist = []
 
 vocabulary = []
 
 letter = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26,'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26, '.':0, ' ':0, '-':0, '\'':0}
-
-
 
 def consist():
     book = openpyxl.load_workbook('7000.xlsx')
@@ -27,33 +25,9 @@ def consist():
             point += letter[alphabet[total]]
         if point == 100:
             vocabulary.append(spilt)
-    delduplicates = set(vocabulary)
+    delduplicates = set(vocabulary)  #<----轉成set時候才發生亂數的情況
     print(delduplicates)
 
-def printme( name ):
-    userinput = list(name)
-    record = 0
-    for firstword in range(0, len(userinput)):
-        try:
-            record += letter[userinput[firstword]]
-        except Exception as e:
-            print("your input", e, "is a not Qualification Character Symbol that can't count point")
-    print(name, "is point total :",record)
+consist()
 
-def main():
-    print("choosen one you wanna to execute.")
-    print("1.pickup a word from file thath qualified convert point special point on user ")
-    print("2.import and process 7000's word and show special point in file")
-    choosen = input("which number is you want to exect : ")
-    try:
-        if int(choosen) == 1:
-            name = str(input("Enter Identify Word: "))
-            printme(name)
-        elif int(choosen) == 2:
-            consist()
-        else:
-            print("choosen neither 1 nor 2, please choosen again")
-    except Exception as e:
-        print(e, ",This filed restricttyped to only accepted numeric")
 
-main()
